9234c3d60d9f6edc9b7fba65cb31c5bf008a6ccd
Hallee Wong <hallee@mit.edu>
2023-12-15 21:34:00 -0500

    add basic model code, add weights link

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # ScribblePrompt
 
-### [Project Page](https://scribbleprompt.csail.mit.edu) | [Demo](https://huggingface.co/spaces/halleewong/ScribblePrompt) | [Video](https://youtu.be/L8CiAoHzPUE)
+### [Project Page](https://scribbleprompt.csail.mit.edu) | [Paper](https://arxiv.org/abs/2312.07381) | [Demo](https://huggingface.co/spaces/halleewong/ScribblePrompt) | [Video](https://youtu.be/L8CiAoHzPUE)
 
 Official implementation of "ScribblePrompt: Fast and Flexible Interactive Segmentation for any Medical Image" 
 
-[Hallee E. Wong](https://halleewong.github.io/), [Marianne Rakic](https://mariannerakic.github.io/), [John V. Guttag](https://people.csail.mit.edu/guttag/), [Adrian V. Dalca](http://www.mit.edu/~adalca/)
+[Hallee E. Wong](https://halleewong.github.io/), [Marianne Rakic](https://mariannerakic.github.io/), [John Guttag](https://people.csail.mit.edu/guttag/), [Adrian V. Dalca](http://www.mit.edu/~adalca/)
+
+## Updates
+
+🚨 **This repo is under construction!** 🚨 Please check back for updates
 
 ## Overview
 
@@ -16,34 +20,75 @@ Official implementation of "ScribblePrompt: Fast and Flexible Interactive Segmen
 
 ## Models
 
-We provide model weights for two versions of ScribblePrompt: 
+We provide [checkpoints](https://www.dropbox.com/scl/fo/zl12obhnsqc2mq7ulviq9/h?rlkey=suaj632fd9aqd6c2gtajz1ywc&dl=0) for two versions of ScribblePrompt: 
 
-* **ScribblePrompt Unet** with an efficient fully-convolutional architecture  
+* **ScribblePrompt-UNet** with an efficient fully-convolutional architecture  
 
-* **ScribblePrompt SAM** based on the [Segment Anything Model](https://github.com/facebookresearch/segment-anything)
+* **ScribblePrompt-SAM** based on the [Segment Anything Model](https://github.com/facebookresearch/segment-anything)
 
-Both models have been trained with iterative **click, bounding box and scribble interactions** on a diverse collection of 65 medical imaging datasets. 
+Both models have been trained with iterative **click, bounding box and scribble interactions** on a diverse collection of 65 medical imaging datasets with both real and synthetic labels. 
 
 ## Installation
 
 You can install `scribbleprompt` in two ways:
 
-* With pip
+* **With pip**:
 
 ```
 pip install git+https://github.com/halleewong/ScribblePrompt.git
 ```
 
-* Manually: cloning it and installing dependencies
+* **Manually**: cloning it and installing dependencies
 ```
 git clone https://github.com/halleewong/ScribblePrompt
 python -m pip install -r ./ScribblePrompt/requirements.txt
-export PYTHONPATH="$PYTHONPATH:$(relpath ./ScribblePrompt)"
+export PYTHONPATH="$PYTHONPATH:$(realpath ./ScribblePrompt)"
 ```
-See [here](https://github.com/facebookresearch/segment-anything#installation) for Segment Anything's dependencies.
 
-## Ongoing
+## Getting Started
 
+First, download the model [checkpoints](https://www.dropbox.com/scl/fo/zl12obhnsqc2mq7ulviq9/h?rlkey=suaj632fd9aqd6c2gtajz1ywc&dl=0) to `./checkpoints`.
+
+To instantiate ScribblePrompt-UNet and make a prediction:
+```
+from scribbleprompt import ScribblePromptUNet
+
+sp_unet = ScribblePromptUNet()
+
+mask = sp_unet.predict(
+    image,        # (B, 1, H, W) 
+    point_coords, # (B, n, 2)
+    point_labels, # (B, n)
+    scribbles,    # (B, 2, H, W)
+    box,          # (B, n, 4)
+    mask_input,   # (B, 1, H, W)
+) # -> (B, 1, H, W) 
+```
+
+To instantiate ScribblePrompt-SAM and make a prediction:
+```
+from scribbleprompt import ScribblePromptSAM
+
+sp_sam = ScribblePromptSAM()
+
+mask, img_features, low_res_logits = sp_sam.predict(
+    image,        # (B, 1, H, W) 
+    point_coords, # (B, n, 2)
+    point_labels, # (B, n)
+    scribbles,    # (B, 2, H, W)
+    box,          # (B, n, 4)
+    mask_input,   # (B, 1, 256, 256)
+) # -> (B, 1, H, W), (B, 16, 256, 256), (B, 1, 256, 256)
+
+```
+`image` should have spatial dimensions $(H,W) = (128,128)$ and pixel values min-max normalized to the $[0,1]$ range. 
+
+For ScribblePrompt-UNet, `mask_input` should be the logits from the previous prediction. For ScribblePrompt-SAM, `mask_input` should be `low_res_logits` from the previous prediction. 
+
+## To Do
+
+- [x] Release Gradio demo 
+- [x] Release model code and weights
 - [ ] Release jupyter notebook tutorial
 - [ ] Release scribble simulation code
 - [ ] Release segmentation labels collected using ScribblePrompt
@@ -52,10 +97,22 @@ See [here](https://github.com/facebookresearch/segment-anything#installation) fo
 
 Code for ScribblePrompt SAM builds on [Segment Anything](https://github.com/facebookresearch/segment-anything) 
 
+## Reference
+
+If you find our work or any of our materials useful, please cite our paper:
+```
+@article{wong2023scribbleprompt,
+  title={ScribblePrompt: Fast and Flexible Interactive Segmentation for Any Medical Image},
+  author={Hallee E. Wong and Marianne Rakic and John Guttag and Adrian V. Dalca},
+  journal={arXiv:2312.07381},
+  year={2023},
+}
+```
+
 ## License
 
 This project is released under the [Apache 2.0 License](https://github.com/halleewong/ScribblePrompt/blob/main/LICENSE) 
 
-## Citation
+
 
 
--- scribbleprompt/unet.py
+++ scribbleprompt/unet.py
@@ -1,22 +1,28 @@
+import os
 from typing import Literal, Tuple, Optional, Dict
 import torch
+import torch.nn.functional as F
 
-from network import UNet
+from .network import UNet
 
 
 class ScribblePromptUNet:
 
     weights = {
-        "v1": "../checkpoints/scribbleprompt_unet_v1_res128.pt"
+        "v1": "./checkpoints/ScribblePrompt_unet_v1_nf192_res128.pt"
     }
     
-    def __init__(self, version: Literal["v1"] = "v1", pretrained: bool = True, device = None) -> None:
+    def __init__(self, version: Literal["v1"] = "v1", device = None) -> None:
         
+        if device is None:
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+
         self.version = version
         self.device = device
-        self.build_model(pretrained=pretrained)
+        self.build_model(pretrained=True)
+        self.input_size = (128,128)
     
-    def build_model(self, pretrained: bool):
+    def build_model(self, pretrained: bool = True):
         """
         Build model
         """
@@ -24,11 +30,11 @@ class ScribblePromptUNet:
             in_channels = 5,
             out_channels = 1,
             features = [192, 192, 192, 192],
-        )
+        ).to(self.device)
         if pretrained:
-            #state = torch.hub.load_state_dict_from_url(self.weights[self.version])
-
             checkpoint_file = self.weights[self.version]
+            assert os.path.exists(checkpoint_file), f"Checkpoint file not found: {checkpoint_file}. Please download from Dropbox"
+            
             with open(checkpoint_file, "rb") as f:
                 state = torch.load(f, map_location=self.device)
             
@@ -39,22 +45,93 @@ class ScribblePromptUNet:
         self.device = device
 
     @torch.no_grad()
-    def predict(img: torch.Tensor, 
+    def predict(self,
+                img: torch.Tensor, # B x 1 x H x W
                 point_coords: Optional[torch.Tensor] = None, # B x n x 2
                 point_labels: Optional[torch.Tensor] = None, # B x n 
-                scribbles = None, # B x 2 x H x W
-                box = None, # B x 1 x 4
+                scribbles: Optional[torch.Tensor] = None, # B x 2 x H x W
+                box: Optional[torch.Tensor] = None, # B x 1 x 4
                 mask_input = None, # B x 1 x H x W
+                return_logits: bool = False,
                 ):
+        """
+        Make predictions from pytorch tensor inputs with grayscale images
+        If the tensors are on the GPU it will prepare the inputs on GPU and retun the mask on the GPU
+
+        Note: if batch size > 1, the number of clicks/boxes must be the same for each image in the batch
+
+        Args:
+            img: torch.Tensor (B x 1 x H x W) image to segment on [0,1] scale
+            point_coords: torch.Tensor (B x n x 2) coordinates of pos/neg clicks in [x,y] format
+            point_labels: torch.Tensor (B x n) labels of clicks (0 or 1)
+            scribbles: torch.Tensor (B x 2 x H x W) pos/neg scribble inputs
+            box: torch.Tensor (B x 1 x 4) bounding box inputs in [x1, y1, x2, y2] format
+            mask_input: torch.Tensor (B x 1 x 128 x 128) logits of previous prediction
+            return_logits: bool, if True return logits instead of mask on [0,1] scale
         
-        pass
+        Returns:
+            mask (torch.Tensor): B x 1 x H x W prediction for each image in batch
+        
+        """
+        assert (len(img.shape)==4) and (img.shape[1]==1), f"img shape should be B x 1 x H x W. current shape: {img.shape}"
+        assert img.min() >= 0 and img.max() <= 1, f"img should be on [0,1] scale. current range: {img.min()} {img.max()}"
 
+        prompts = {
+            'img': img,
+            'point_coords': point_coords,
+            'point_labels': point_labels,
+            'scribbles': scribbles,
+            'box': box,
+            'mask_input': mask_input,
+        }
+        # Prepare inputs for ScribblePrompt unet (B x 5 x H x W)
+        x = prepare_inputs(prompts).float().to(self.device)
 
+        yhat = self.model(x)
+
+        # B x 1 x H x W
+        if return_logits:
+            return yhat
+        else:
+            return torch.sigmoid(yhat)
+
+    
 # -----------------------------------------------------------------------------
 # Prepare inputs
 # -----------------------------------------------------------------------------
 
-def prepare_inputs(self, inputs: Dict[str,torch.Tensor], device = None) -> torch.Tensor:
+def rescale_inputs(inputs: Dict[str,any], input_size: Tuple[int] = (128,128)):
+    """
+    Rescale the inputs 
+    """ 
+    h,w = inputs['img'].shape[-2:]
+    if [h,w] != input_size:
+        
+        inputs.update(dict(
+            img = F.interpolate(inputs['img'], size=input_size, mode='bilinear')
+        ))
+
+        if inputs.get('scribbles') is not None:
+            inputs.update({
+                'scribbles': F.interpolate(inputs['scribbles'], size=input_size, mode='bilinear') 
+            })
+        
+        if inputs.get("box") is not None:
+            boxes = inputs.get("box").clone()
+            coords = boxes.reshape(-1, 2, 2)
+            coords[..., 0] = coords[..., 0] * (input_size[1] / w)
+            coords[..., 1] = coords[..., 1] * (input_size[0] / h)
+            inputs.update({'box': coords.reshape(1, -1, 4).int()})
+        
+        if inputs.get("point_coords") is not None:
+            coords = inputs.get("point_coords").clone()
+            coords[..., 0] = coords[..., 0] * (input_size[1] / w)
+            coords[..., 1] = coords[..., 1] * (input_size[0] / h)
+            inputs.update({'point_coords': coords.int()})
+
+    return inputs
+
+def prepare_inputs(inputs: Dict[str,torch.Tensor], device = None) -> torch.Tensor:
     """
     Prepare inputs for network
 
@@ -83,10 +160,10 @@ def prepare_inputs(self, inputs: Dict[str,torch.Tensor], device = None) -> torch
     else:
         scribble_click_embed = torch.zeros((img.shape[0], 2) + shape, device=device)
 
-    if inputs.get("scribble") is not None:
+    if inputs.get("scribbles") is not None:
         # Combine scribbles with click embedding
         # B x 2 x H x W
-        scribble_click_embed = torch.clamp(scribble_click_embed + inputs.get('scribble'), min=0.0, max=1.0)
+        scribble_click_embed = torch.clamp(scribble_click_embed + inputs.get('scribbles'), min=0.0, max=1.0)
 
     if inputs.get('mask_input') is not None:
         # Previous prediction
