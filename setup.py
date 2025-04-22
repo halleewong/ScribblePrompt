86becd349b21eda8a8ba8b932303333cb3ce7382
Sébastien Goll <sebastien.goll@kitware.com>
2025-04-22 15:58:01 +0200

    [DEMO]: Add voxynth to "training" extras

--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@ from setuptools import find_packages, setup
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+with open("requirements_training.txt") as f:
+    requirements_training = f.read().splitlines()
+
 setup(
     name="scribbleprompt",
     version="0.1.0",
     python_requires=">=3.9",
     packages=find_packages(exclude="notebooks"),
     install_requires=requirements,
+    extras_require={
+        "training": requirements_training,
+    }
 )
